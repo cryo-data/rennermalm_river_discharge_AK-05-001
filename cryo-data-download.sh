85e9412e4b658fad039b4f960c647dab3046cb7f
Kenneth D. Mankoff <mankoff@gmail.com>
2022-06-03 09:12:55 -0700

    Updated with new cryo-data internal format

--- cryo-data-download.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-datalad download-url https://doi.pangaea.de/10.1594/PANGAEA.762898?format=textfile
-
---
+++ cryo-data/download.sh
@@ -0,0 +1,1 @@
+../.git/annex/objects/X5/W0/MD5E-s105--2a34e71659fdaea5d01f0ebf882a1322.sh/MD5E-s105--2a34e71659fdaea5d01f0ebf882a1322.sh
